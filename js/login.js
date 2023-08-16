0bcee3abc8b5ced2e0baa6244878e42de2d9112d
matiasribadulla <141092939+matiasribadulla@users.noreply.github.com>
2023-08-15 23:06:00 -0300

    Mejoras
    Función anónima y nueva variable

--- js/login.js
+++ js/login.js
@@ -1,5 +1,6 @@
 document.getElementById("btn").addEventListener("click", function(event) {
   event.preventDefault(); // Evitar el envío automático del formulario
+document.getElementById("btn").addEventListener("click", () => {
 
   var username = document.getElementById("emailinput").value;
   var password = document.getElementById("password").value;
@@ -7,6 +8,7 @@ document.getElementById("btn").addEventListener("click", function(event) {
   if (username === "" || password === "") {
       alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
   } else {
+      localStorage.setItem("logeado", true);
       location.replace("index.html");
   }
 });
