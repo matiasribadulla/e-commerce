57f8e7b2e83ccf90cae97b7157afc5a2f641c462
matiasribadulla <141092939+matiasribadulla@users.noreply.github.com>
2023-08-25 19:33:30 -0300

    Entrega 2

--- js/products.js
+++ js/products.js
@@ -1,44 +1,30 @@
-const ARTICULOS = "https://japceibal.github.io/emercado-api/cats_products/"+ localStorage.getItem("catID") +".json";
-const ARTICULOS = "https://japceibal.github.io/emercado-api/cats_products/"+localStorage.getItem("catID")+".json";
+const ARTICULOS = "https://japceibal.github.io/emercado-api/cats_products/"+localStorage.getItem("catID")+".json"
 
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
 const ORDER_ASC_BY_PRICE = "$A";
 const ORDER_DESC_BY_PRICE = "$D";
 const ORDER_BY_PROD_SOLDCOUNT = "Sold";
 let productsArray = [];
-let currentSortCriteria = undefined;
 let ordenado = [];
 let minCount = undefined;
 let maxCount = undefined;
 
 function sortProducts(criteria, array){
     let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
     if (criteria === ORDER_ASC_BY_PRICE)
     {
         result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
             if ( a.cost < b.cost ){ return -1; }
             if ( a.cost > b.cost ){ return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_DESC_BY_NAME){
     }else if (criteria === ORDER_DESC_BY_PRICE){
         result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
             if ( a.cost > b.cost ){ return -1; }
             if ( a.cost < b.cost ){ return 1; }
             return 0;
         });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
     }else if (criteria === ORDER_BY_PROD_SOLDCOUNT){
         result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
             let aCount = parseInt(a.soldCount);
             let bCount = parseInt(b.soldCount);
 
@@ -85,12 +71,9 @@ function showProductsList(array){
 }
 
 function sortAndShowCategories(sortCriteria){
-    currentSortCriteria = sortCriteria;
-
-    productsArray.products = sortProducts(currentSortCriteria, productsArray.products);
+    
     ordenado = sortProducts(sortCriteria, productsArray.products);
 
-    showProductsList(productsArray);
     showProductsList(ordenado);
 }
 
@@ -105,17 +88,14 @@ document.addEventListener("DOMContentLoaded", function(e){
     });
 
     document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_ASC_BY_NAME);
         sortAndShowCategories(ORDER_ASC_BY_PRICE);
     });
 
     document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_DESC_BY_NAME);
         sortAndShowCategories(ORDER_DESC_BY_PRICE);
     });
 
     document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowCategories(ORDER_BY_PROD_COUNT);
         sortAndShowCategories(ORDER_BY_PROD_SOLDCOUNT);
     });
 
@@ -126,13 +106,10 @@ document.addEventListener("DOMContentLoaded", function(e){
         minCount = undefined;
         maxCount = undefined;
 
-        showCategoriesList();
         showProductsList(productsArray.products);
     });
 
     document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
-        //de productos por categoría.
         minCount = document.getElementById("rangeFilterCountMin").value;
         maxCount = document.getElementById("rangeFilterCountMax").value;
 
@@ -157,34 +134,9 @@ document.addEventListener("DOMContentLoaded", function(e){
 const searchInput = document.getElementById('search-input');
 const searchResults = document.getElementById('search-results');
 
-searchInput.addEventListener('input', function() {
-  const searchText = searchInput.value.toLowerCase();
-
-  searchResults.innerHTML = '';
-
-  Promise.all([loadData('data1.json'), loadData('data2.json')])
-    .then(results => {
-      const allData = results.flat();
-
-      const matchingResults = allData.filter(item => item.nombre.toLowerCase().includes(searchText));
 searchInput.addEventListener('input', ()=> {
   let searchText = searchInput.value.toLowerCase();
 
-      matchingResults.forEach(result => {
-        const resultItem = document.createElement('div');
-        resultItem.textContent = result.nombre;
-        searchResults.appendChild(resultItem);
-      });
-    });
   let a = productsArray.products.filter(product => product.name.toLowerCase().includes(searchText));
   showProductsList(a);
 });
-
-function loadData(jsonFile) {
-  return fetch(jsonFile)
-    .then(response => response.json())
-    .catch(error => {
-      console.error('Error al cargar el archivo JSON:', error);
-      return [];
-    });
-}
