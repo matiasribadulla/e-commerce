8007d227865e9af1782f9fdbc1549490fc749358
matiasribadulla <141092939+matiasribadulla@users.noreply.github.com>
2023-11-09 22:04:07 -0300

    Entrega 7
    Comentarios y cambio de nombre tanto en variables como funciones

--- js/init.js
+++ js/init.js
@@ -40,68 +40,75 @@ let getJSONData = function(url){
     });
 };
 
+//Funcion para agregar los botones en el nav
 document.addEventListener("DOMContentLoaded", ()=>{
-  let lista = document.getElementById("navbarNav").getElementsByClassName("nav-item");
+  let list = document.getElementById("navbarNav").getElementsByClassName("nav-item");
 
-  lista[lista.length-1].innerHTML = `
+  list[list.length-1].innerHTML = `
     <div class="dropdown">
       <a class="btn btn-secondary dropdown-toggle" href="my-profile.html" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-        ${localStorage.getItem("logeado")}
+        ${localStorage.getItem("logged")}
       </a>
 
       <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
         <li><a class="dropdown-item" href="cart.html">Mi carrito</a></li>
         <li><a class="dropdown-item" href="my-profile.html">Mi perfil</a></li>
-        <li><button class="dropdown-item" onclick="cambio()">Cambiar Modo</button></li>
-        <li><a class="dropdown-item" href="login.html" onclick="cerrar()">Cerrar sesión</a></li>
+        <li><button class="dropdown-item" onclick="changeColor()">Cambiar Modo</button></li>
+        <li><a class="dropdown-item" href="login.html" onclick="unlog()">Cerrar sesión</a></li>
       </ul>
     </div>`;
-    setTimeout(colorPag,175);
+    setTimeout(colorPag,150);
 });
 
-function cambio(){
-  let page = document.body;
-  let divs = document.getElementsByTagName("div");
-  let lis = document.getElementsByTagName("li")
-  page.classList.toggle("dark");
-  for(let i=0;i<divs.length;i++){
-    divs[i].classList.toggle("dark");
-  }
-  for(let i=0;i<lis.length;i++){
-    lis[i].classList.toggle("dark");
-  }
+//Funcion para cambiar la pagina entre modo claro-oscuro
+function changeColor(){
+    let page = document.body;
+    let divs = document.getElementsByTagName("div");
+    let lis = document.getElementsByTagName("li");
+    page.classList.toggle("dark");
+    //Cambia el color de cada div
+    for(let i=0;i<divs.length;i++){
+      divs[i].classList.toggle("dark");
+    }
+    //Cambia el color de cada li
+    for(let i=0;i<lis.length;i++){
+      lis[i].classList.toggle("dark");
+    }
 
-  //
-  if(document.body.classList.contains("dark")){
-      localStorage.setItem('dark-mode','true');
-  } else {
-      localStorage.setItem('dark-mode','false');
-  };
+    //Guarda en el localstorage si el modo oscuro esta activo o no
+    if(document.body.classList.contains("dark")){
+        localStorage.setItem('dark-mode','true');
+    } else {
+        localStorage.setItem('dark-mode','false');
+    };
 };
 
+// Funcion que cambia el color de la pagina segun lo guardado de tu sesion anterior
 function colorPag(){
+  //Verifica en el localstorage si el modo oscuro estaba activo o no
 if(localStorage.getItem("dark-mode") === "true"){
-  document.body.classList.add("dark")
-  let divs = document.getElementsByTagName("div")
-  let lis = document.getElementsByTagName("li")
-  for(let i=0;i<divs.length;i++){
-    divs[i].classList.add("dark");
-  }
-  for(let i=0;i<lis.length;i++){
-    lis[i].classList.add("dark");
-  }
+    document.body.classList.add("dark")
+    let divs = document.getElementsByTagName("div")
+    let lis = document.getElementsByTagName("li")
+    for(let i=0;i<divs.length;i++){
+      divs[i].classList.add("dark");
+    }
+    for(let i=0;i<lis.length;i++){
+      lis[i].classList.add("dark");
+    }
 } else {
-  document.body.classList.remove("dark")
-  let divs = document.getElementsByTagName("div")
-  let lis = document.getElementsByTagName("li")
-  for(let i=0;i<divs.length;i++){
-    divs[i].classList.remove("dark");
-  }
-  for(let i=0;i<coments.length;i++){
-    lis[i].classList.remove("dark");
-  }
-}}
+    document.body.classList.remove("dark")
+    let divs = document.getElementsByTagName("div")
+    let lis = document.getElementsByTagName("li")
+    for(let i=0;i<divs.length;i++){
+      divs[i].classList.remove("dark");
+    }
+    for(let i=0;i<lis.length;i++){
+      lis[i].classList.remove("dark");
+    }
+}};
 
-function cerrar(){
-localStorage.removeItem("logeado");
-}
+// Funcion para cerrar sesion
+function unlog(){
+  localStorage.removeItem("logged");
+};
